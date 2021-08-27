d71df3155ae9dbe025e4475661a98d9e4c2fb2d6
Andrey Omrlchenko <46873647+zetronik@users.noreply.github.com>
2021-08-27 12:21:37 +0300

    update search service

--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -15,15 +15,49 @@ export class SearchService {
     return of(data).pipe(
       debounceTime(1000),
       filter(data => data.length >= 2),
-      mergeMap(data => this.http.post('https://places-dsn.algolia.net/1/places/query', JSON.stringify({query: data}))),
-      map(data => <SearchResponse[]>data)
-    )
+      mergeMap(data =>
+        this.http.post(
+          'https://places-dsn.algolia.net/1/places/query',
+          JSON.stringify({ query: data }),
+          {
+            headers: {
+              ['Accept']:
+                'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              ['Accept-Encoding']: 'gzip, deflate, br',
+              ['Accept-Language']:
+                'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6,la;q=0.5,de;q=0.4',
+              ['Sec-Fetch-Site']: 'cross-site',
+              ['Sec-Fetch-Mode']: 'cors',
+              ['Sec-Fetch-Dest']: 'empty',
+              ['Host']: 'places-dsn.algolia.net',
+              ['Connection']: 'keep-alive'
+            }
+          }
+        )
+      ),
+      map(data => <SearchResponse[]>data.hits)
+    );
   }
 
   public getObjectId(id: string): Observable<SearchResponse> {
-    return this.http.get('https://places-dsn.algolia.net/1/places/' + id).pipe(
-      map(data => <SearchResponse>data),
-      delay(300)
-    )
+    return this.http
+      .get('https://places-dsn.algolia.net/1/places/' + id, {
+        headers: {
+          ['Accept']:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          ['Accept-Encoding']: 'gzip, deflate, br',
+          ['Accept-Language']:
+            'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6,la;q=0.5,de;q=0.4',
+          ['Sec-Fetch-Site']: 'cross-site',
+          ['Sec-Fetch-Mode']: 'cors',
+          ['Sec-Fetch-Dest']: 'empty',
+          ['Host']: 'places-dsn.algolia.net',
+          ['Connection']: 'keep-alive'
+        }
+      })
+      .pipe(
+        map(data => <SearchResponse>data),
+        delay(300)
+      );
   }
 }
